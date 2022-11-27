295b0a327b361eb1759b0171dd61d53b2cf0d4f5
tursunmurod <nodejsbackand@gmail.com>
2022-11-28 00:21:57 +0500

    funtions add sort

--- main/main.js
+++ main/main.js
@@ -1,1 +1,15 @@
 let array=[1,3,2,6,4,9,7,8];
+function Sort(arr) {
+let len = arr.length; 
+for (let i=0; i<len; i++) {
+    for (let j=0; j<len; j++) {
+        if (arr[j] > arr[j+1]) {
+            let tem  = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] = tem;
+        }
+    }
+}
+  return arr;  
+};
+console.log(Sort(array));
